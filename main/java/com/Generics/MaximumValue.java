521b8b02705dd8cb1318c37e46549cd02315974f
renu100 <renu.chandraker08@gmail.com>
2021-10-04 11:44:40 +0530

    Added: Take 3 integer value and find max and Write test case

--- main/java/com/Generics/MaximumValue.java
+++ main/java/com/Generics/MaximumValue.java
@@ -1,10 +1,18 @@
 package com.Generics;
 
 public class MaximumValue {
-	 public static void main(String[] args) {
-		 System.out.println("Welcome");
-		
+	public Integer findMax(Integer value1, Integer value2, Integer value3) {
+		Integer max = value1;
+		if (value2.compareTo(max) > 0) {
+			max = value2;
+		}
+		if (value3.compareTo(max) > 0) {
+			max = value3;
+		}
+		System.out.println("Maximum Integer number is " + max);
+		return max;
 	}
+
 	}
 	
 
--- test/java/com/Generics/MaximumValueTest.java
+++ test/java/com/Generics/MaximumValueTest.java
@@ -4,18 +4,30 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MaximumValueTest {
+MaximumValue max=new MaximumValue();
 	
-	MaximumValue max=new MaximumValue();
-
 	@Test
-	public void welcome() {
-		int value = max.findMax(welcome);
+	public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtFirstPosition() {
+		int value = max.findMax(9, 5, 6);
 		Assert.assertEquals(9, value);
 	}
 
+	@Test
+	public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtSecondPosition() {
+		int value = max.findMax(5, 9, 6);
+		Assert.assertEquals(9, value);
 	}
-	
-	
-	
+
+	@Test
+	public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtThirdPosition() {
+		int value = max.findMax(6, 5, 9);
+		Assert.assertEquals(9, value);
+	}
+
 	
 }
+	
+	
+	
+	
+
