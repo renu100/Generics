19d64cdf7639aff7f63923993a80629846c0872f
renu100 <renu.chandraker08@gmail.com>
2021-10-04 12:58:32 +0530

    Added: Take 3 float value and find max and Write test case

--- main/java/com/Generics/MaximumValue.java
+++ main/java/com/Generics/MaximumValue.java
@@ -1,6 +1,8 @@
 package com.Generics;
 
 public class MaximumValue {
+
+	// Find integer maximum value
 	public Integer findMax(Integer value1, Integer value2, Integer value3) {
 		Integer max = value1;
 		if (value2.compareTo(max) > 0) {
@@ -13,6 +15,17 @@ public class MaximumValue {
 		return max;
 	}
 
+	// Find float maximum value
+	public Float findMax(Float value1, Float value2, Float value3) {
+		Float max = value1;
+		if (value2.compareTo(max) > 0) {
+			max = value2;
+		}
+		if (value3.compareTo(max) > 0) {
+			max = value3;
+		}
+		System.out.println("Maximum Float number is " + max);
+		return max;
 	}
-	
 
+}
--- test/java/com/Generics/MaximumValueTest.java
+++ test/java/com/Generics/MaximumValueTest.java
@@ -4,8 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MaximumValueTest {
-MaximumValue max=new MaximumValue();
-	
+	MaximumValue max = new MaximumValue();
+
 	@Test
 	public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtFirstPosition() {
 		int value = max.findMax(9, 5, 6);
@@ -24,10 +24,22 @@ MaximumValue max=new MaximumValue();
 		Assert.assertEquals(9, value);
 	}
 
-	
-}
-	
-	
-	
-	
+	@Test
+	public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxAtFirstPosition() {
+		float value = max.findMax(9.0f, 5.0f, 6.0f);
+		Assert.assertEquals(9.0f, value, 0.0f);
+	}
 
+	@Test
+	public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxSecoundPosition() {
+		float value = max.findMax(5.0f, 9.0f, 6.0f);
+		Assert.assertEquals(9.0f, value, 0.0f);
+	}
+
+	@Test
+	public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxThirdPosition() {
+		float value = max.findMax(9.0f, 5.0f, 11.0f);
+		Assert.assertEquals(11.0f, value, 0.0f);
+	}
+
+}
