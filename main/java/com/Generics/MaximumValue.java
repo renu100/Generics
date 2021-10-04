20782eceab1ee1204377846678345c4f3218be6e
renu100 <renu.chandraker08@gmail.com>
2021-10-05 01:38:04 +0530

    Added: Refector all 3 method to one generic method

--- main/java/com/Generics/MaximumValue.java
+++ main/java/com/Generics/MaximumValue.java
@@ -2,9 +2,9 @@ package com.Generics;
 
 public class MaximumValue {
 
-	// Find integer maximum value
-	public Integer findMax(Integer value1, Integer value2, Integer value3) {
-		Integer max = value1;
+	// Generic Method
+	public <T extends Comparable<T>> T findMax(T value1, T value2, T value3) {
+		T max = value1;
 		if (value2.compareTo(max) > 0) {
 			max = value2;
 		}
@@ -15,30 +15,8 @@ public class MaximumValue {
 		return max;
 	}
 
-	// Find float maximum value
-	public Float findMax(Float value1, Float value2, Float value3) {
-		Float max = value1;
-		if (value2.compareTo(max) > 0) {
-			max = value2;
-		}
-		if (value3.compareTo(max) > 0) {
-			max = value3;
-		}
-		System.out.println("Maximum Float number is " + max);
-		return max;
-	}
+	
 
-	// Find String maximum value
-	public String findMax(String value1, String value2, String value3) {
-		String max = value1;
-		if (value2.compareTo(max) > 0) {
-			max = value2;
-		}
-		if (value3.compareTo(max) > 0) {
-			max = value3;
-		}
-		System.out.println("Maximum String number is " + max);
-		return max;
-	}
+	
 
 }
