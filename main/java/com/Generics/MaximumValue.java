a1a6b3435a3bb216cb75080590be4be5443aa431
renu100 <renu.chandraker08@gmail.com>
2021-10-12 15:54:27 +0530

    Added: Take more then three parameters

--- main/java/com/Generics/MaximumValue.java
+++ main/java/com/Generics/MaximumValue.java
@@ -1,29 +1,25 @@
 package com.Generics;
 
 public class MaximumValue<T extends Comparable<T>> {
+	T[] values;
 
-	T value1, value2, value3;
-
-	public MaximumValue(T value1, T value2, T value3) {
-		this.value1 = value1;
-		this.value2 = value2;
-		this.value3 = value3;
+	// create a parameterized constructor
+	public MaximumValue(T[] values) {
+		this.values = values;
 	}
 
-	public static <T extends Comparable<T>> T testMaximum(T value1, T value2, T value3) {
-		T max = value1;
-		if (value2.compareTo(max) > 0) {
-			max = value2;
-		}
-		if (value3.compareTo(max) > 0) {
-			max = value3;
+	// create a method which take a multiple value
+	public static <T extends Comparable<T>> T testMaximum(T... values) {
+		T max = values[0];
+		for (T t : values) {
+			if (t.compareTo(max) > 0)
+				max = t;
 		}
 		System.out.println("Maximum value is " + max);
 		return max;
 	}
 
 	public T findMax() {
-		return MaximumValue.testMaximum(value1, value2, value3);
+		return MaximumValue.testMaximum(values);
 	}
-
 }
--- test/java/com/Generics/MaximumValueTest.java
+++ test/java/com/Generics/MaximumValueTest.java
@@ -3,59 +3,77 @@ package com.Generics;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MaximumValueTest {
+class MaximumValueTestCase {
+
+	// Integer type User input value and find maximum value at first position
 	@Test
 	public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtFirstPosition() {
-		int value = new MaximumValue<Integer>(9, 5, 4).findMax();
+		Integer[] arr = { 9, 3, 2, 4, 5, 2 };
+		int value = new MaximumValue<Integer>(arr).findMax();
 		Assert.assertEquals(9, value);
 	}
 
+	// Integer type User input value and find maximum value at second position
 	@Test
 	public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtSecondPosition() {
-		int value = new MaximumValue<Integer>(5, 9, 6).findMax();
+		Integer[] arr = { 3, 9, 2, 4, 5, 2 };
+		int value = new MaximumValue<Integer>(arr).findMax();
 		Assert.assertEquals(9, value);
 	}
 
+	// Integer type User input value and find maximum value at third position
 	@Test
 	public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtThirdPosition() {
-		int value = new MaximumValue<Integer>(6, 5, 9).findMax();
+		Integer[] arr = { 3, 2, 9, 5, 2 };
+		int value = new MaximumValue<Integer>(arr).findMax();
 		Assert.assertEquals(9, value);
 	}
 
+	// Float type User input value and find maximum value at first position
 	@Test
 	public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxAtFirstPosition() {
-		float value = new MaximumValue<Float>(9.0f, 5.0f, 6.0f).findMax();
+		Float[] arr = { 9.0f, 5.0f, 6.0f };
+		float value = new MaximumValue<Float>(arr).findMax();
 		Assert.assertEquals(9.0f, value, 0.0f);
 	}
+	// Float type User input value and find maximum value at second position
 
 	@Test
 	public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxSecoundPosition() {
-		float value = new MaximumValue<Float>(5.0f, 9.0f, 6.0f).findMax();
+		Float[] arr = { 5.0f, 9.0f, 6.0f };
+		float value = new MaximumValue<Float>(arr).findMax();
 		Assert.assertEquals(9.0f, value, 0.0f);
 	}
 
+	// Float type User input value and find maximum value at third position
 	@Test
 	public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxThirdPosition() {
-		float value = new MaximumValue<Float>(9.0f, 5.0f, 11.0f).findMax();
+		Float[] arr = { 9.0f, 5.0f, 11.0f };
+		float value = new MaximumValue<Float>(arr).findMax();
 		Assert.assertEquals(11.0f, value, 0.0f);
 	}
 
+	// String type User input value and find maximum value at first position
 	@Test
 	public void givenThreeNumber_WhenStringValue_ShouldReturnMaxAtFirstPosition() {
-		String value = new MaximumValue<String>("peach", "apple", "banana").findMax();
+		String[] arr = { "peach", "apple", "banana" };
+		String value = new MaximumValue<String>(arr).findMax();
 		Assert.assertEquals("peach", value);
 	}
 
+	// String type User input value and find maximum value at second position
 	@Test
 	public void givenThreeNumber_WhenStringValue_ShouldReturnMaxAtSecoundPosition() {
-		String value = new MaximumValue<String>("apple", "peach", "banana").findMax();
+		String[] arr = { "apple", "peach", "banana" };
+		String value = new MaximumValue<String>(arr).findMax();
 		Assert.assertEquals("peach", value);
 	}
 
+	// String type User input value and find maximum value at third position
 	@Test
 	public void givenThreeNumber_WhenStringValue_ShouldReturnMaxAtThirdPosition() {
-		String value = new MaximumValue<String>("apple", "banana", "peach").findMax();
+		String[] arr = { "apple", "banana", "peach" };
+		String value = new MaximumValue<String>(arr).findMax();
 		Assert.assertEquals("peach", value);
 	}
-
 }
